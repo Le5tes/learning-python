cbda2f2b7e9cfb7776d8113beff5857df7bd5c87
Tim Williamson <32397024+Le5tes@users.noreply.github.com>
2018-03-01 16:56:47 +0000

    Adds responses to allcaps

--- grandma.py
+++ grandma.py
@@ -1,2 +1,7 @@
+import random
 def tell_grandma(something):
-	return 'WHAT?! SPEAK UP SONNY!'
+	if something.isupper():
+		year = str(int(round(random.random() * 40 + 1930)))
+		return 'NO, NOT SINCE ' + year + '!'
+	else:
+		return 'WHAT?! SPEAK UP SONNY!'
--- grandma_test.py
+++ grandma_test.py
@@ -1,4 +1,13 @@
 import grandma
+import random
+
 def test_grandma_is_a_bit_deaf():
 	response = grandma.tell_grandma('hi grandma')
 	assert response == 'WHAT?! SPEAK UP SONNY!'
+
+def test_she_responds_to_all_caps():
+	random.seed(10)
+	response = grandma.tell_grandma('HI GRANDMA')
+	assert response == 'NO, NOT SINCE 1953!'
+
+	
